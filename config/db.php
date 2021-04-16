5581a0029f841571d535af477f4946da2d10cf2a
Cristhian Moya <crmoya@gmail.com>
2021-04-16 13:17:02 -0400

    cambios de servidor de bd

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=aq4ai38z_firmador',
-    'username' => 'aq4ai38z_firmador',
+    'dsn' => 'mysql:host=localhost;dbname=cristhia_firmador',
+    'username' => 'cristhia_firmador',
     'password' => 'K~8fT}Xso+hL',
     'charset' => 'utf8',
 
