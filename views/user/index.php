51c7ab7d2381314c8e23fa3886bdbd4c96560195
Cristhian Moya <crmoya@gmail.com>
2021-08-09 18:59:48 -0400

    agregado index de users

---
+++ views/user/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Usuarios';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Crear nuevo usuario', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'username',
+            'name',
+            'rol',
+
+            ['class' => 'yii\grid\ActionColumn','template' => '{view} {delete}'],
+        ],
+    ]); ?>
+
+
+</div>
