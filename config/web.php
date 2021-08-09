5b5539a111cddc04670f601a560b31678008fa03
Cristhian Moya <crmoya@gmail.com>
2021-08-09 16:56:17 -0400

    obviar index.php

--- config/web.php
+++ config/web.php
@@ -51,10 +51,7 @@ $config = [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-                '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',   			
+            'rules' => [			
             ],
         ],
         'authManager' => [
